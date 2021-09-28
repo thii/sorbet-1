fe0f5b2c0ac130b7afedd0a6fa0add0a26cba528
Trevor Elliott <awesomelyawesome@gmail.com>
2021-09-27 17:15:10 -0700

    Finish porting uses of rb_iterate

--- compiler/IREmitter/Payload/codegen-payload.c
+++ compiler/IREmitter/Payload/codegen-payload.c
@@ -2029,7 +2029,7 @@ VALUE sorbet_inlineIntrinsicEnv_apply(VALUE value, BlockConsumerFFIType intrinsi
 
 SORBET_INLINE
 VALUE sorbet_callIntrinsicInlineBlock(VALUE (*body)(VALUE), VALUE recv, ID fun, int argc, VALUE *argv, BlockFFIType blk,
-                                      VALUE closure) {
+                                      int blkMinArgs, int blkMaxArgs, VALUE closure) {
     struct sorbet_inlineIntrinsicEnv env;
     env.recv = recv;
     env.fun = fun;
@@ -2040,12 +2040,12 @@ VALUE sorbet_callIntrinsicInlineBlock(VALUE (*body)(VALUE), VALUE recv, ID fun, 
     // NOTE: we pass the block function to rb_iterate so that we ensure that the block handler is setup correctly.
     // However it won't be called through the vm, as that would hide the direct call to the block function from the
     // inliner.
-    return rb_iterate(body, (VALUE)&env, blk, closure);
+    return sorbet_rb_iterate(body, (VALUE)&env, blk, blkMinArgs, blkMaxArgs, closure);
 }
 
 SORBET_INLINE
 VALUE sorbet_callIntrinsicInlineBlock_noBreak(VALUE (*body)(VALUE), VALUE recv, ID fun, int argc, VALUE *argv,
-                                              BlockFFIType blk, VALUE closure) {
+                                              BlockFFIType blk, int blkMinArgs, int blkMaxArgs, VALUE closure) {
     struct sorbet_inlineIntrinsicEnv env;
     env.recv = recv;
     env.fun = fun;
@@ -2061,7 +2061,7 @@ VALUE sorbet_callIntrinsicInlineBlock_noBreak(VALUE (*body)(VALUE), VALUE recv, 
     rb_execution_context_t *ec = GET_EC();
     rb_control_frame_t *cfp = ec->cfp;
 
-    const struct vm_ifunc *const ifunc = rb_vm_ifunc_proc_new(blk, (void *)closure);
+    const struct vm_ifunc *const ifunc = rb_vm_ifunc_new(blk, (void *)closure, blkMinArgs, blkMaxArgs);
     struct rb_captured_block *captured = (struct rb_captured_block *)&cfp->self;
     captured->code.ifunc = ifunc;
     VALUE blockHandler = VM_BH_FROM_IFUNC_BLOCK(captured);
@@ -2169,7 +2169,7 @@ static VALUE sorbet_iterSuper(VALUE obj) {
 
 SORBET_INLINE
 VALUE sorbet_callSuperBlock(int argc, SORBET_ATTRIBUTE(noescape) const VALUE *const restrict argv, int kw_splat,
-                            BlockFFIType blockImpl, VALUE closure) {
+                            BlockFFIType blockImpl, int blkMinArgs, int blkMaxArgs, VALUE closure) {
     // Mostly an implementation of return rb_call_super(argc, argv);
     rb_execution_context_t *ec = GET_EC();
     VALUE recv = ec->cfp->self;
@@ -2198,7 +2198,7 @@ VALUE sorbet_callSuperBlock(int argc, SORBET_ATTRIBUTE(noescape) const VALUE *co
     arg.me = me;
     arg.kw_splat = kw_splat;
 
-    return rb_iterate(sorbet_iterSuper, (VALUE)&arg, blockImpl, closure);
+    return sorbet_rb_iterate(sorbet_iterSuper, (VALUE)&arg, blockImpl, blkMinArgs, blkMaxArgs, closure);
 }
 
 SORBET_INLINE
